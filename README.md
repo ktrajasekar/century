08424172ae91cbb5bb2557e8487032d9c2f210aa
Dev <kt.rajasekar@gmail.com>
2017-05-24 13:14:46 +0530

    Menu

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# century
-century
+# Notes
+
+Plugins
+1.) Menu - https://toddmotto.com/flaunt-js-for-stylish-responsive-navigations-with-nested-click-to-reveal/
+2.) Smooth Scroll v4.7.0 * Animate scrolling to anchor links, by Chris Ferdinandi - http://gomakethings.com
---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(function(){
+    "use strict";
+    // Full screen pre loader
+    $(window).load(function(){
+        $("#pre-loader").delay(500).fadeOut(2000);
+        $(".preload-logo").addClass('zoomOutUp');
+        $(".loader").addClass('zoomOutDown');
+    });
+
+
+    //Logo fadeIn fadeOut on
+    $(window).scroll(function(){
+      if($(this).scrollTop() > $(window).height()/2.2) $('.logo-wrapper').fadeOut('slow');
+      if($(this).scrollTop() < $(window).height()/2.2) $('.logo-wrapper').fadeIn('slow');
+    });
+
+    //Using the smooth scroll for smooth navigation
+    smoothScroll.init({
+        speed: 500, // Integer. How fast to complete the scroll in milliseconds
+        easing: 'easeInOutCubic', // Easing pattern to use
+        updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+        offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+        callbackBefore: function ( toggle, anchor ) {}, // Function to run before scrolling
+        callbackAfter: function ( toggle, anchor ) {} // Function to run after scrolling
+    });
+
+    // //wow animation by using with animate css
+    // var isiPad = (navigator.userAgent.match(/iPad/i) != null);
+    // if (($.browser.mobile)||(isiPad)) {
+    //     // disable animation on mobile
+    //     $("body").removeClass("wow");
+    // }
+    // else{
+    //     var wow = new WOW(
+    //       {
+    //         boxClass:     'wow',
+    //         animateClass: 'animated',
+    //         offset:       0,
+    //         mobile:       true
+    //       }
+    //     );
+    //     wow.init();
+    // }
+});
