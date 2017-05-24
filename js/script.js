e2ec37521490617bdbff38c4858023f483298d1b
Dev <kt.rajasekar@gmail.com>
2017-05-24 23:36:14 +0530

    Update

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ $(function(){
 
     //Using the smooth scroll for smooth navigation
     smoothScroll.init({
-        speed: 500, // Integer. How fast to complete the scroll in milliseconds
+        speed: 1000, // Integer. How fast to complete the scroll in milliseconds
         easing: 'easeInOutCubic', // Easing pattern to use
         updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
         offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
