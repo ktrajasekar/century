1151da3dbd5f83b800fe581c3aea84b2a7029e0d
Rajasekhar <kt.rajasekar@gmail.com>
2017-06-02 21:48:09 +0530

    Update

--- README.md
+++
@@ -1,5 +0,0 @@
-# Notes
-
-Plugins
-1.) Menu - https://toddmotto.com/flaunt-js-for-stylish-responsive-navigations-with-nested-click-to-reveal/
-2.) Smooth Scroll v4.7.0 * Animate scrolling to anchor links, by Chris Ferdinandi - http://gomakethings.com
--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@ $(function(){
         $("#pre-loader").delay(500).fadeOut(2000);
         $(".preload-logo").addClass('zoomOutUp');
         $(".loader").addClass('zoomOutDown');
-    });
+    });   
+    
 
-
-    //Logo fadeIn fadeOut on
+    //Logo fadeIn fadeOut on 
     $(window).scroll(function(){
       if($(this).scrollTop() > $(window).height()/2.2) $('.logo-wrapper').fadeOut('slow');
       if($(this).scrollTop() < $(window).height()/2.2) $('.logo-wrapper').fadeIn('slow');
     });
 
-    //Using the smooth scroll for smooth navigation
+    //Using the smooth scroll for smooth navigation    
     smoothScroll.init({
-        speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+        speed: 500, // Integer. How fast to complete the scroll in milliseconds
         easing: 'easeInOutCubic', // Easing pattern to use
         updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
         offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
@@ -33,53 +33,164 @@ $(function(){
     else{
         var wow = new WOW(
           {
-            boxClass:     'wow',
-            animateClass: 'animated',
-            offset:       0,
-            mobile:       true
+            boxClass:     'wow',      
+            animateClass: 'animated', 
+            offset:       0,          
+            mobile:       true        
           }
         );
         wow.init();
     }
 
-    /* Demo Scripts for Bootstrap Carousel and Animate.css article
-* on SitePoint by Maria Antonietta Perna
-*/
-(function( $ ) {
+    // Full screen navigations
+    var triggerBttn = document.getElementById( 'trigger-navbar' ),
+        navbar = document.querySelector( 'section.navbar' ),
+        closeBttn = navbar.querySelector( 'a.navbar-close' ),
+        navClick = navbar.querySelector( 'section.navbar nav ul li a' ),
+        transEndEventNames = {
+            'WebkitTransition': 'webkitTransitionEnd',
+            'MozTransition': 'transitionend',
+            'OTransition': 'oTransitionEnd',
+            'msTransition': 'MSTransitionEnd',
+            'transition': 'transitionend'
+        },
+        transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+        support = { transitions : Modernizr.csstransitions };
 
-	//Function to animate slider captions
-	function doAnimations( elems ) {
-		//Cache the animationend event in a variable
-		var animEndEv = 'webkitAnimationEnd animationend';
+        function toggleOverlay() {
+        if( classie.has( navbar, 'open' ) ) {
+            classie.remove( navbar, 'open' );
+            classie.add( navbar, 'close' );
+            var onEndTransitionFn = function( ev ) {
+                if( support.transitions ) {
+                    if( ev.propertyName !== 'visibility' ) return;
+                    this.removeEventListener( transEndEventName, onEndTransitionFn );
+                }
+                classie.remove( navbar, 'close' );
+            };
+            if( support.transitions ) {
+                navbar.addEventListener( transEndEventName, onEndTransitionFn );
+            }
+            else {
+                onEndTransitionFn();
+            }
+        }
+        else if( !classie.has( navbar, 'close' ) ) {
+            classie.add( navbar, 'open' );
+        }
+    }
 
-		elems.each(function () {
-			var $this = $(this),
-				$animationType = $this.data('animation');
-			$this.addClass($animationType).one(animEndEv, function () {
-				$this.removeClass($animationType);
-			});
-		});
-	}
+    triggerBttn.addEventListener( 'click', toggleOverlay );
+    closeBttn.addEventListener( 'click', toggleOverlay );    
+    $('section.navbar nav ul li a').click(function(){
+        toggleOverlay();
+    });
 
-	//Variables on page load
-	var $myCarousel = $('#carousel-example-generic'),
-		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+    //prepare video
+    $('.video').height($(window).height());
+    $('.home-text').css('top',$(window).height()/4+'px');
+    $('.home-text-2').css('top',$(window).height()/4.5+'px');
 
-	//Initialize carousel
-	$myCarousel.carousel();
+    $(window).resize(function() {
+        $('.video').height($(window).height());
+        $('.home-text').css('top',$(window).height()/4+'px');
+        $('.home-text-2').css('top',$(window).height()/4.5+'px');
+    });
+    $('.video .cont').addClass('visible');
+    
+   
+    
 
-	//Animate captions in first slide on page load
-	doAnimations($firstAnimatingElems);
+  
+    /*/ grid gallery (Portfolio)
+    new CBPGridGallery( document.getElementById( 'grid-gallery' ) );*/
 
-	//Pause carousel
-	$myCarousel.carousel('pause');
+    // Carousel
+    $("#home-text-slider").owlCarousel({ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      autoPlay : 5000,
+      stopOnHover : false,
+      paginationSpeed : 400,
+      singleItem:true       
+    });
 
+    $("#testimonial-slider").owlCarousel({ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      autoPlay : 5000,
+      stopOnHover : false,
+      paginationSpeed : 400,
+      singleItem:true       
+    });
 
-	//Other slides to be animated on carousel slide event
-	$myCarousel.on('slide.bs.carousel', function (e) {
-		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
-		doAnimations($animatingElems);
-	});
+    
+    //Counter Up
+    $('.counter').counterUp({
+        delay: 5,
+        time: 800
+    });
 
-})(jQuery);
+   
+
+    // Ajax working contact form
+    $("#submit").click(function() { 
+        //get input field values
+        var user_name       = $('input[name=name]').val(); 
+        var user_email      = $('input[name=email]').val();
+        var user_phone      = $('input[name=phone]').val();
+        var user_message    = $('textarea[name=message]').val();
+        var post_data;
+        var output;
+        
+        //simple validation at client's end
+        //we simply change border color to red if empty field using .css()
+        var proceed = true;
+        if(user_name==""){ 
+            $('input[name=name]').css('border-color','red'); 
+            proceed = false;
+        }
+        if(user_email==""){ 
+            $('input[name=email]').css('border-color','red'); 
+            proceed = false;
+        }
+        if(user_phone=="") {    
+            $('input[name=phone]').css('border-color','red'); 
+            proceed = false;
+        }
+        if(user_message=="") {  
+            $('textarea[name=message]').css('border-color','red'); 
+            proceed = false;
+        }
+
+        //everything looks good! letsproceed
+        if(proceed) 
+        {
+            //data to be sent to server
+            post_data = {'userName':user_name, 'userEmail':user_email, 'userPhone':user_phone, 'userMessage':user_message};
+            
+            //Ajax post data to server
+            $.post('include/contact.php', post_data, function(response){  
+                
+                //load json data from server and output message     
+                if(response.type == 'error')
+                {
+                    output = '<div class="error text-center">'+response.text+'</div>';
+                }else{
+                
+                    output = '<div class="success text-center">'+response.text+'</div>';
+                    
+                    //reset values in all input fields
+                    $('#contact_form input').val(''); 
+                    $('#contact_form textarea').val(''); 
+                }
+                
+                $("#form_result").hide().html(output).slideDown();
+            }, 'json');
+            
+        }
+    });
+    
+  
+
 });
