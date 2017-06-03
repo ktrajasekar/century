09753bb06b8d66537b170675249e8a341e245156
Rajasekhar <kt.rajasekar@gmail.com>
2017-06-03 20:34:22 +0530

    update

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ $(function(){
         $("#pre-loader").delay(500).fadeOut(2000);
         $(".preload-logo").addClass('zoomOutUp');
         $(".loader").addClass('zoomOutDown');
-    });   
-    
+    });
 
-    //Logo fadeIn fadeOut on 
+
+    //Logo fadeIn fadeOut on
     $(window).scroll(function(){
       if($(this).scrollTop() > $(window).height()/2.2) $('.logo-wrapper').fadeOut('slow');
       if($(this).scrollTop() < $(window).height()/2.2) $('.logo-wrapper').fadeIn('slow');
     });
 
-    //Using the smooth scroll for smooth navigation    
+    //Using the smooth scroll for smooth navigation
     smoothScroll.init({
         speed: 500, // Integer. How fast to complete the scroll in milliseconds
         easing: 'easeInOutCubic', // Easing pattern to use
@@ -33,10 +33,10 @@ $(function(){
     else{
         var wow = new WOW(
           {
-            boxClass:     'wow',      
-            animateClass: 'animated', 
-            offset:       0,          
-            mobile:       true        
+            boxClass:     'wow',
+            animateClass: 'animated',
+            offset:       0,
+            mobile:       true
           }
         );
         wow.init();
@@ -81,7 +81,7 @@ $(function(){
     }
 
     triggerBttn.addEventListener( 'click', toggleOverlay );
-    closeBttn.addEventListener( 'click', toggleOverlay );    
+    closeBttn.addEventListener( 'click', toggleOverlay );
     $('section.navbar nav ul li a').click(function(){
         toggleOverlay();
     });
@@ -97,100 +97,101 @@ $(function(){
         $('.home-text-2').css('top',$(window).height()/4.5+'px');
     });
     $('.video .cont').addClass('visible');
-    
-   
-    
 
-  
+
+
+
+
     /*/ grid gallery (Portfolio)
     new CBPGridGallery( document.getElementById( 'grid-gallery' ) );*/
 
     // Carousel
-    $("#home-text-slider").owlCarousel({ 
+    $("#home-text-slider").owlCarousel({
       navigation : true, // Show next and prev buttons
       slideSpeed : 300,
       autoPlay : 5000,
       stopOnHover : false,
       paginationSpeed : 400,
-      singleItem:true       
+      singleItem:true
     });
 
-    $("#testimonial-slider").owlCarousel({ 
+    $("#products-slider").owlCarousel({
       navigation : true, // Show next and prev buttons
       slideSpeed : 300,
       autoPlay : 5000,
+      items:3,
       stopOnHover : false,
       paginationSpeed : 400,
-      singleItem:true       
+      singleItem:true
     });
 
-    
+
     //Counter Up
     $('.counter').counterUp({
         delay: 5,
         time: 800
     });
 
-   
+
 
     // Ajax working contact form
-    $("#submit").click(function() { 
+    $("#submit").click(function() {
         //get input field values
-        var user_name       = $('input[name=name]').val(); 
+        var user_name       = $('input[name=name]').val();
         var user_email      = $('input[name=email]').val();
         var user_phone      = $('input[name=phone]').val();
         var user_message    = $('textarea[name=message]').val();
         var post_data;
         var output;
-        
+
         //simple validation at client's end
         //we simply change border color to red if empty field using .css()
         var proceed = true;
-        if(user_name==""){ 
-            $('input[name=name]').css('border-color','red'); 
+        if(user_name==""){
+            $('input[name=name]').css('border-color','red');
             proceed = false;
         }
-        if(user_email==""){ 
-            $('input[name=email]').css('border-color','red'); 
+        if(user_email==""){
+            $('input[name=email]').css('border-color','red');
             proceed = false;
         }
-        if(user_phone=="") {    
-            $('input[name=phone]').css('border-color','red'); 
+        if(user_phone=="") {
+            $('input[name=phone]').css('border-color','red');
             proceed = false;
         }
-        if(user_message=="") {  
-            $('textarea[name=message]').css('border-color','red'); 
+        if(user_message=="") {
+            $('textarea[name=message]').css('border-color','red');
             proceed = false;
         }
 
         //everything looks good! letsproceed
-        if(proceed) 
+        if(proceed)
         {
             //data to be sent to server
             post_data = {'userName':user_name, 'userEmail':user_email, 'userPhone':user_phone, 'userMessage':user_message};
-            
+
             //Ajax post data to server
-            $.post('include/contact.php', post_data, function(response){  
-                
-                //load json data from server and output message     
+            $.post('include/contact.php', post_data, function(response){
+
+                //load json data from server and output message
                 if(response.type == 'error')
                 {
                     output = '<div class="error text-center">'+response.text+'</div>';
                 }else{
-                
+
                     output = '<div class="success text-center">'+response.text+'</div>';
-                    
+
                     //reset values in all input fields
-                    $('#contact_form input').val(''); 
-                    $('#contact_form textarea').val(''); 
+                    $('#contact_form input').val('');
+                    $('#contact_form textarea').val('');
                 }
-                
+
                 $("#form_result").hide().html(output).slideDown();
             }, 'json');
-            
+
         }
     });
-    
-  
+
+
 
 });
