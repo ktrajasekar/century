518e0c6e9ba3019b6c2901f3f73277059aad9223
Dev <kt.rajasekar@gmail.com>
2017-06-06 21:56:33 +0530

    update

--- index.php
+++ index.php
@@ -401,7 +401,7 @@ made of high quality materials</p>
 <script type="text/javascript" src="js/vendor/respond.js"></script>
 <script type="text/javascript" src="js/vendor/jquery.counterup.js"></script>
 <script type="text/javascript" src="js/vendor/waypoints.min.js"></script>
-<!-- <script type="text/javascript" src="js/vendor/smoothscroll.js"></script></script> -->
+<script type="text/javascript" src="js/vendor/smoothscroll.js"></script></script>
 <script type="text/javascript" src="js/vendor/jquery.backstretch.min.js"></script>
 <script type="text/javascript" src="js/vendor/wow.js"></script>
 <script type="text/javascript" src="js/vendor/detectmobilebrowser.js"></script>
